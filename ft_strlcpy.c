11e2f33cb3ecf56d91f1f798f817bee4af7cf30a
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-10 01:01:46 +0300

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -14,5 +14,15 @@
 
 size_t ft_strlcpy (char *dst, const char *src, size_t size)
 {
+  unsigned int	i;
 
+	if (dst == NULL || src == NULL)
+		return (0);
+	if (size == 0)
+		return (ft_strlen(src));
+	i = -1;
+	while (++i < size - 1 && src[i] != '\0')
+		dst[i] = src[i];
+	dst[i] = '\0';
+	return (ft_strlen(src));
 }
