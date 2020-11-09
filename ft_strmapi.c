8e4f070a6f2bca9103b0820b42f720cb7798e788
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-10 01:02:30 +0300

    Update ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -14,5 +14,21 @@
 
 char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+  size_t	i;
+	size_t	len;
+	char	*mapped;
 
+	if (s == NULL || f == NULL)
+		return (NULL);
+	len = ft_strlen(s);
+	if ((mapped = (char*)malloc(sizeof(char) * (len + 1))) == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len)
+	{
+		mapped[i] = (*f)((unsigned int)i, s[i]);
+		i++;
+	}
+	mapped[i] = '\0';
+	return (mapped);
 }
