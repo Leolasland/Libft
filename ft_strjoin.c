49737c32597e7387b0ca6380dfeebddbf83450ee
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-10 01:00:32 +0300

    Update ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -12,7 +12,31 @@
 
 #include "libft.h"
 
-char *ft_strjoin(char const *s1, char const *s2)
+char	*ft_strsjoin(char **strs, char *delim)
 {
+	int		i;
+	size_t	join_len;
+	size_t	delim_len;
+	char	*join;
 
+	delim_len = ft_strlen(delim);
+	join_len = 0;
+	i = -1;
+	while (strs[++i] != NULL)
+	{
+		join_len += ft_strlen(strs[i]);
+		if (strs[i + 1] != NULL)
+			join_len += delim_len;
+	}
+	if ((join = (char*)malloc(sizeof(char) * (join_len + 1))) == NULL)
+		return (NULL);
+	join[0] = '\0';
+	i = -1;
+	while (strs[++i] != NULL)
+	{
+		ft_strcat(join, strs[i]);
+		if (*strs[i] != '\0' && strs[i + 1] != NULL)
+			ft_strcat(join, delim);
+	}
+	return (join);
 }
