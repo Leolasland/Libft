c36011c5a2ffc32152c0664bf6b5622ee731423e
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-10 01:03:50 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -1,1 +1,29 @@
-/* ************************************************************************** *//*                                                                            *//*                                                        :::      ::::::::   *//*   ft_strnstr.c                                       :+:      :+:    :+:   *//*                                                    +:+ +:+         +:+     *//*   By: hdanyel <hdanyel@student.42.fr>            +#+  +:+       +#+        *//*                                                +#+#+#+#+#+   +#+           *//*   Created: 2020/11/07 12:57:45 by hdanyel           #+#    #+#             *//*   Updated: 2020/11/07 12:59:05 by hdanyel          ###   ########.fr       *//*                                                                            *//* ************************************************************************** */#include "libft.h"char * ft_strnstr (const char *big, const char *little, size_t len){}
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hdanyel <hdanyel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/07 12:57:45 by hdanyel           #+#    #+#             */
+/*   Updated: 2020/11/07 12:59:05 by hdanyel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char * ft_strnstr (const char *big, const char *little, size_t len)
+{
+  size_t	needle_len;
+
+	needle_len = ft_strlen(needle);
+	if (needle_len == 0)
+		return ((char*)haystack);
+	while (*haystack && len-- >= needle_len)
+	{
+		if (ft_strnequ(haystack, needle, needle_len))
+			return ((char*)haystack);
+		haystack++;
+	}
+	return (NULL);
+}
