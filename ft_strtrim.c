9bf8272f0dc4c546b1b4ca0076d4183c2c9f16eb
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-10 01:04:50 +0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -14,5 +14,18 @@
 
 char *ft_strtrim(char const *s1, char const *set)
 {
+  size_t	start;
+	size_t	len;
 
+	if (s1 == NULL || set == NULL)
+		return (NULL);
+	start = 0;
+	while (s1[start] && ft_strchr(set, s1[start]) != NULL)
+		start++;
+	len = ft_strlen(&s1[start]);
+	if (len != 0)
+		while (s1[start + len - 1]
+				&& ft_strchr(set, s1[start + len - 1]) != NULL)
+			len--;
+	return (ft_strsub(s1, start, len));
 }
