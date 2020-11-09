9977c14d7bce91f068ced0760e641d97290ffda8
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-10 00:50:48 +0300

    Update ft_strchr.c

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,5 +14,12 @@
 
 char *ft_strchr(const char *str, int ch)
 {
+  char	*ptr;
+	char	sym;
 
+	ptr = (char *)s;
+	sym = (char)c;
+	while (*ptr && !(*ptr == sym))
+		ptr++;
+	return ((*ptr == sym) ? ptr : NULL);
 }
