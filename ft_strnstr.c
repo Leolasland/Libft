254ebdf7a1e6dd22804312d1bae1212d5bfe127a
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-15 16:04:51 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -14,5 +14,25 @@
 
 char * ft_strnstr (const char *big, const char *little, size_t len)
 {
+  char	*h;
+	size_t	little_len;
+	size_t	j;
+	size_t	i;
 
+	h = (char *)big;
+	if (!(little_len = ft_strlen(little)))
+		return (h);
+	if (ft_strlen(big) < little_len || len < little_len)
+		return (NULL);
+	i = 0;
+	while (h[i] && i <= len - little_len)
+	{
+		j = 0;
+		while (little[j] && little[j] == h[i + j])
+			j++;
+		if (j == little_len)
+			return (&h[i]);
+		i++;
+	}
+	return (NULL);
 }
