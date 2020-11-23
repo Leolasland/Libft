8e60c573a6ca009a88699ff0f699ab9bbfce5f4f
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-24 00:22:11 +0300

    Update ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -12,10 +12,45 @@
 
 #include "libft.h"
 
+static char	*ft_strchr(const char *s, int c)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (*ptr != c)
+	{
+		if (*ptr == '\0')
+		{
+			return (0);
+		}
+		ptr++;
+	}
+	return (ptr);
+}
+
+static char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		++i;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
 char *ft_strtrim(char const *s1, char const *set)
 {
-  size_t	start;
+  	size_t	start;
 	size_t	len;
+	char *new;
 
 	if (s1 == NULL || set == NULL)
 		return (NULL);
@@ -24,8 +59,11 @@ char *ft_strtrim(char const *s1, char const *set)
 		start++;
 	len = ft_strlen(&s1[start]);
 	if (len != 0)
-		while (s1[start + len - 1]
-				&& ft_strchr(set, s1[start + len - 1]) != NULL)
+		while (s1[start + len - 1] != 0 && ft_strchr(set, s1[start + len - 1]) != NULL)
 			len--;
-	return (ft_strsub(s1, start, len));
+	if (!(new = malloc(sizeof(char) * len + 1)))
+		return (NULL);
+	ft_strncpy(new, s1 + star, len);
+	new[len] = '\0';
+	return (new);
 }
