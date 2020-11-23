fc0f97a872d4837edd9db6d5dea9fde4bb5655a8
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-23 22:27:02 +0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -14,19 +14,24 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-  	size_t	i;
-	size_t	j;
-	size_t	dst_len;
-	size_t	src_len;
+  	size_t dstlen;
+	size_t srclen;
+	size_t len;
 
-	dst_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	if (size <= dst_len)
-		return (src_len + size);
-	i = 0;
-	j = dst_len;
-	while (src[i] && j < size - 1)
-		dst[j++] = src[i++];
-	dst[j] = '\0';
-	return (dst_len + src_len);
+	dstlen = ft_strlen(dst);
+	srclen = 0;
+	if (dstlen < n)
+		len = dstlen;
+	else
+		len = n;
+	if (n != 0)
+		--n;
+	while (src[srclen] != '\0' && dstlen < n)
+	{
+		dst[dstlen] = src[srclen];
+		srclen++;
+		dstlen++;
+	}
+	dst[dstlen] = '\0';
+	return (len + ft_strlen(src));
 }
