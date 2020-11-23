fc45ba90f3e9ce4a0e3123ed0f598e45aba09fac
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-23 18:52:13 +0300

    Update ft_memccpy.c

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -14,17 +14,15 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	unsigned char	sym;
 	unsigned char	*d;
 	unsigned char	*s;
 
-	sym = (unsigned char)c;
 	d = (unsigned char *)dst;
 	s = (unsigned char *)src;
 	while (n--)
 	{
 		*d++ = *s++;
-		if (*(d - 1) == sym)
+		if (*s == (unsigned char) c)
 			return (d);
 	}
 	return (NULL);
