c570fca131f90399b4df0e1828a7c717d016e8a3
Leolasland <73824563+Leolasland@users.noreply.github.com>
2020-11-23 23:33:22 +0300

    Update ft_split.c

--- ft_split.c
+++ ft_split.c
@@ -14,76 +14,76 @@
 
 static	int	 ft_sum(const char *str, char c)
 {
-	int	q;
+	int	word;
 ​
-	q = 0;
+	word = 0;
+	if (*str !=c && *str)
+	{
+		str++;
+		word++;
+	}
  	while (*str != '\0')
 	{
 		while (*str == c)
 		{
 			str++;
-			if (*str == '\0')
-				return(q);
+			if (*str !=c && *str)
+				word++;
 		}
-		if (*str != c)
-			q++;
- 		while (*str != c)
-		{
-			str++;
-			if (*str == '\0')
-				return (q);
-		}
+		str++;
 	}
-	return (q);
+	return (word);
 }
 ​
-static	int	ft_separation (const char *s, char c)
+static	int	ft_separation (const char *str, char c)
 {
-	int	len;
+	int	count;
 ​
-	len = 0;
-	while (*s != c && *s != '\0')
+	count = 0;
+	while (*str != c && *str != '\0')
 	{
-		s++;
-		len++;
+		str++;
+		count++;
 	}
-	return (len);
+	return (count);
 }
 ​
-static	void	ft_free(char **arr, int k)
+static	void	ft_free(char **arr)
 {
-	while (k--)
-		free(arr[k]);
+	int i;
+	
+	i = 0;
+	while (arr[i])
+		free(arr[i++]);
 	free(arr);
 }
 ​
 char		**ft_split(char const *s, char c)
 {
 	char	**arr;
-	int	j;
-	int	k;
 	int	i;
-	int	sum;
+	int	j;
 ​
 	i = 0;
-	sum = ft_sum(s, c);
-	if (!s || !((arr = (char **)malloc((sum + 1) * sizeof(char *)))))
+	j = 0;
+	if (!s || (!(arr = (char **)malloc(sizeof(char *) * (ft_sum(s, c) + 1)))))
 		return (NULL);
-	k = -1;
-	while (k++ != sum)
+	while (*s)
 	{
-		while (s[i] == c && s[i] != '\0')
-			i++;
-		if (!(arr[k] = (char*)malloc(sizeof(char) * (ft_separation(&s[i], c) + 1))))
+		while (*s == c && *s)
+			s++;
+		if (*s !=c && *s)
 		{
-			ft_free(arr, k);
-			return (NULL);
+			if (!(arr[j] = (char*)malloc(sizeof(char) * (ft_separation(s, c) + 1))))
+				return (ft_free(arr));
+			while (*s != c && *s)
+				arr[j][i++] = *s++;
+			arr[j][i] '\0';
+			j++;
+			i = 0;
 		}
-		arr[k][ft_separation(&s[i], c)] = '\0';
-		j = 0;
-		while (s[i] != c && s[i] != '\0')
-			arr[k][j++] = s[i++];
+		
 	}
-	arr[k+1] = NULL;
+	arr[j] = NULL;
 	return (arr);
 }
